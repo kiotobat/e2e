34e31eda3fc6ebf5f51cd09abde3fe2f29cacbba
kiotobat <lambar926@gmail.com>
2025-07-31 16:57:51 +0300

    redit jest

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   target: "web",
@@ -43,6 +44,11 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'licenses.txt', to: '.' }
+      ],
     }),
   ],
 };
